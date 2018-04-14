787e73667b52f3382a4552df44935e988e6ca569
Hanool21 <37235527+Hanool21@users.noreply.github.com>
2018-04-14 03:01:21 -0700

    HW 4

--- exercises.js
+++ exercises.js
@@ -11,14 +11,13 @@
 
 var numBurgers = 5;
 var restaurant = 'In n Out';
+var friend = 'Katherine,';
+var day = 'Friday';
 
   // We will also help you with part of the result, so that you know what it sort of looks like. REMEMBER, this is not the complete solution. 
   // This is what the final output should look like: You will be eating _____ burgers at this restaurant: _____ with my friend, _____, on this day: ______
 
-var questionOneAnswer = 'You will be eating ' + numBurgers + ' burgers at this restaurant: ' + restaurant;
-
-
-
+var questionOneAnswer = 'You will be eating ' + numBurgers + ' burgers at this restaurant: ' + restaurant + ' with my friend, ' + friend + ' on this day: ' + day;
 
 //-----------------------------
 // #2 The Age Calculator 
@@ -28,32 +27,31 @@ var questionOneAnswer = 'You will be eating ' + numBurgers + ' burgers at this r
   //You will have a total of 3 variables: the current year, the birth year, and a calculation based on the 2 years.
   // Calculate their 2 possible ages based on the stored values. 
 
-
 // Store your answer here
-var questionTwoAnswer = '';
+var year = 2018;
+var birthyear = 1994;
 
-
-
+var questionTwoAnswer = 'They are ' + (year - birthyear) + ' or ' + (year - birthyear - 1);
 
 //-----------------------------
 // #3 The Temperature Converter
 //-----------------------------
-
-
+var celcius = 17;
+var farenheit = 62;
+var symbol = '\u00B0';
 // Celsius to Farenheit Answer
-var questionThreeAnswerA = '';
+var questionThreeAnswerA = celcius + symbol + ' degrees Celcius is ' + ((celcius*9/5) +32) + symbol + ' degrees Farenheit.';
 
 // Farenheit to Celsius Answer
-var questionThreeAnswerB = '';
-
+var questionThreeAnswerB = farenheit + symbol + ' degrees Farenheit is ' + ((farenheit-32)*5/9) + symbol + ' degrees Celcius';
 
 
 
 //-----------------------------
 // #4 The Bonus Question
 //-----------------------------
-
-var questionFourAnswer = '';
+var farenheit1 = Math.random() + 99;
+var questionFourAnswer = farenheit1 + symbol + ' degrees Farenheit is ' + ((farenheit1-32)*5/9) + symbol + ' degrees Celcius';
 
 
 
